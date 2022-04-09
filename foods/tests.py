31f80028ed1b5be0efc835ba577609e3d5fe303d
eclipsium <eclipsium@yandex.ru>
2022-04-09 20:37:21 +0700

    Добавлены тесты

--- foods/tests.py
+++ foods/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
 
-# Create your tests here.
+from foods.models import Food
+from foods.models import FoodCategory
+
+
+class FoodTestCase(TestCase):
+    def setUp(self):
+        # Должна быть 1 кола
+        FoodCategory.objects.create(name_ru="Напитки")
+        # Пустая категория, не дожна отображаться
+        FoodCategory.objects.create(name_ru="Салаты")
+        # Категория с неопубликованной едой, не должна отображаться
+        FoodCategory.objects.create(name_ru="Горячее")
+
+        Food.objects.create(
+            category_id=1,
+            name_ru="Кола",
+            code=100,
+            internal_code=100,
+            cost=123.50,
+            is_publish=True,
+        )
+
+        Food.objects.create(
+            category_id=1,
+            name_ru="Пепси",
+            code=100,
+            internal_code=200,
+            cost=123.50,
+            is_publish=False,
+        )
+
+        Food.objects.create(
+            category_id=3,
+            name_ru="Бургер",
+            code=100,
+            internal_code=300,
+            cost=123.50,
+            is_publish=False,
+        )
+
+        Food.objects.create(
+            category_id=3,
+            name_ru="Картошка фри",
+            code=100,
+            internal_code=400,
+            cost=123.50,
+            is_publish=False,
+        )
+
+    def test_root(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_food_category(self):
+        response = self.client.get("/api/v1/foods/")
+        self.assertEqual(response.status_code, 200)
+
+        # Должна быть всего 1 категория
+        self.assertEqual(len(response.data), 1)
+
+        # И одна еда
+        self.assertEqual(len(response.data[0]["foods"]), 1)
