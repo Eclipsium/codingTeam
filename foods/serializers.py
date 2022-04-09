f56ffb39cc42a5082bb5c50ee81c93a624cb41ef
eclipsium <eclipsium@yandex.ru>
2022-04-09 17:32:30 +0700

    Очистка. Добавление базы

--- foods/serializers.py
+++ foods/serializers.py
@@ -1,23 +1,34 @@
 from rest_framework import serializers
 
-from foods.models import Food, FoodCategory
+from foods.models import Food
+from foods.models import FoodCategory
 
 
 class FoodSerializer(serializers.ModelSerializer):
-    additional = serializers.SlugRelatedField(many=True, read_only=True,
-                                              slug_field='internal_code')
+    additional = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="internal_code"
+    )
 
     class Meta:
         model = Food
         fields = (
-        'internal_code', 'code', 'name_ru', 'description_ru', 'is_publish',
-        'description_en',
-        'description_ch', 'is_vegan', 'is_special', 'cost', 'additional')
+            "internal_code",
+            "code",
+            "name_ru",
+            "description_ru",
+            "description_en",
+            # 'is_publish',
+            "description_ch",
+            "is_vegan",
+            "is_special",
+            "cost",
+            "additional",
+        )
 
 
 class FoodListSerializer(serializers.ModelSerializer):
-    foods = FoodSerializer(source='food', many=True, read_only=True)
+    foods = FoodSerializer(source="food", many=True, read_only=True)
 
     class Meta:
         model = FoodCategory
-        fields = ('id', 'name_ru', 'name_en', 'name_ch', 'order_id', 'foods')
+        fields = ("id", "name_ru", "name_en", "name_ch", "order_id", "foods")
